c6a9f77c47b67262b776717fc16eb4ec9d6031f4
37flwr <nesterishinyarik@gmail.com>
2022-06-26 01:37:09 +0300

    Add basic structure
    Add basic structure; add basic codemirror

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -2,7 +2,17 @@ import './styles.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section id='header'>
+      <div id='header-logo' className='text-heading'>
+        CodeBay
+      </div>
+      <div id='header-buttons'>
+        <button className='header-button'>
+        </button>
+        <button className='header-button'>
+        </button>
+      </div>
+    </section>
   )
 }
 
