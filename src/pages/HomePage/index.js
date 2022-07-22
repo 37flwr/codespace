566e343a7accb2d2f0dbdd9e42fe2f91e2682fd4
37flwr <nesterishinyarik@gmail.com>
2022-07-23 00:05:32 +0300

    Add typewriter

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,9 +1,11 @@
+import Typewriter from "typewriter-effect";
 import DefaultButton from '../../components/Buttons/DefaultButton';
 import { ReactComponent as CodespaceLogo } from '../../assets/CodespaceLogo.svg';
 
 import './styles.scss';
 
 const HomePage = () => {
+
   return (
     <section id='home-page'>
       <div className="hero-part">
@@ -27,14 +29,35 @@ const HomePage = () => {
               + Start a new project
             </span>
             <div className="snippet-logo">
-              <CodespaceLogo className="snippet-logo-icon" />
-              Codespace
+              <CodespaceLogo />
+              <span className="snippet-logo-text">
+                Codespace
+              </span>
             </div>
             <span className="snippet-rightside">
               Profile
             </span>
           </div>
+          <div className="typing-container">
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+              }}
+              onInit={(typewriter)=> {
+              typewriter
+              .typeString(
+                `contract Campaign is Ownable {\n\tuint public minimumContribution;\n\tuint public totalApprovers;\n\n\tstruct Request{\n\t\tstring description;\n\t\tuint value;\n\t\taddress recepient;\n\t\tbool complete;\n\t\tuint approvalCount;\n\t\tuint timestamp;\n\t\tmapping(address => bool) approvals;\n\t}\n\tRequest[] public requests;\n\tmapping(address => bool) public approvers;\n\n\tconstructor(uint minimum) {\n\t\tminimumContribution = minimum;\n\t}\n\n\tfunction contribute() public payable {\n\t\trequire(msg.value > minimumContribution);`
+              )
+              .pauseFor(5000)
+              .deleteAll()
+              .typeString("Welcomes You")
+              .start();
+              }}
+            />
+          </div>
         </div>
+        <div className="snippet-transparent"/>
       </div>
     </section>
   )
