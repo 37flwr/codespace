384e6b027b59eca92844d9e9e7af6cc44f8dc8f1
37flwr <nesterishinyarik@gmail.com>
2023-06-13 19:59:33 +0300

    add basic html markup for bottom snippet

--- src/entities/snippets/BottomSnippet/index.tsx
+++ src/entities/snippets/BottomSnippet/index.tsx
@@ -1,6 +1,16 @@
+import { SquareButton } from '../../../shared/ui/buttons';
 import './styles.scss';
-const BottomSnippet = (): JSX.Element => {
-  return <div>BottomSnippet</div>;
-};
+
+const BottomSnippet = (): JSX.Element => (
+  <div className="bottom-snippet_container">
+    <div className="bottom-snippet_blur" />
+    <div className="bottom-snippet_container_background" />
+    <div className="bottom-snippet">
+      <SquareButton path="/editor" variant="filled">
+        {'</> Code'}
+      </SquareButton>
+    </div>
+  </div>
+);
 
 export default BottomSnippet;
